2e79875429d3187ca760314e66a018e25b33b488
David Benavides <DavidJose.Benavides@gmail.com>
2022-07-12 16:00:34 -0500

    Ajustes varios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,26 +4,26 @@
 
 export const environment = {
   production: false,
-  // firebase: {
-  //   apiKey: 'AIzaSyDGN0Hlm1PAQN5DZdQfZq5tlZp9TeAf45I',
-  //   authDomain: 'tank-english-dev.firebaseapp.com',
-  //   projectId: 'tank-english-dev',
-  //   storageBucket: 'tank-english-dev.appspot.com',
-  //   messagingSenderId: '1056216382366',
-  //   appId: '1:1056216382366:web:15512acb2c886eaa6bf48f'
-  // },
-  // // api: 'http://localhost:5000/tank-english-dev/us-central1'
-  // api: 'https://us-central1-tank-english-dev.cloudfunctions.net'
-
   firebase: {
-    apiKey: 'AIzaSyCJRQTSUJ_DaR8TKxWw0ykluJvf_UjVj1M',
-    authDomain: 'tank-english-prod.firebaseapp.com',
-    projectId: 'tank-english-prod',
-    storageBucket: 'tank-english-prod.appspot.com',
-    messagingSenderId: '226577327419',
-    appId: '1:226577327419:web:e316552b6510df1247431e'
+    apiKey: 'AIzaSyDGN0Hlm1PAQN5DZdQfZq5tlZp9TeAf45I',
+    authDomain: 'tank-english-dev.firebaseapp.com',
+    projectId: 'tank-english-dev',
+    storageBucket: 'tank-english-dev.appspot.com',
+    messagingSenderId: '1056216382366',
+    appId: '1:1056216382366:web:15512acb2c886eaa6bf48f'
   },
-  api: 'https://us-central1-tank-english-prod.cloudfunctions.net'
+  // api: 'http://localhost:5000/tank-english-dev/us-central1'
+  api: 'https://us-central1-tank-english-dev.cloudfunctions.net'
+
+  // firebase: {
+  //   apiKey: 'AIzaSyCJRQTSUJ_DaR8TKxWw0ykluJvf_UjVj1M',
+  //   authDomain: 'tank-english-prod.firebaseapp.com',
+  //   projectId: 'tank-english-prod',
+  //   storageBucket: 'tank-english-prod.appspot.com',
+  //   messagingSenderId: '226577327419',
+  //   appId: '1:226577327419:web:e316552b6510df1247431e'
+  // },
+  // api: 'https://us-central1-tank-english-prod.cloudfunctions.net'
 };
 
 /*
