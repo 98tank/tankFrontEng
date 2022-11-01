de7056335b799e75e73e5d884009ef2186117fe8
David Benavides <DavidJose.Benavides@gmail.com>
2022-11-01 00:24:35 -0600

    Primer acercamiento a la landing

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
@@ -7,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPage implements OnInit {
   positions = ['SAP Funcional', 'Desarrollador Java Sr', 'Desarrollador Saleforce', 'Gerente Omnicanal', 'UX/UI Designer', 'Guidewire Developer', 'Brand Manager', 'On Trade', 'Ciberseguridad', 'Saleforce Architect', 'ABAP HANA', 'Product Owner']
+  form: FormGroup;
+  year = new Date().getFullYear();
 
-  constructor() { }
+  constructor(
+    private formBuild: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.form = this.formBuild.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      subject: ['', [Validators.required]],
+      comments: ['', [Validators.required]],
+    })
+  }
+
+  getData() {
+    console.log(this.form.value);
+  }
+
+  goUrl(url: string) {
+    window.open(url, '_blank')
   }
 
 }
